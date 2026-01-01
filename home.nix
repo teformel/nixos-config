57b2203607b0c3de140a767a14d84d479230e085
maorila <maorila@qq.com>
2026-01-02 04:20:16 +0800

    Feat: Add HomeManager

---
+++ home.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, ... }:
+
+{
+  # 注意：这是必须要有的基本信息
+  home.username = "maorila";
+  home.homeDirectory = "/home/maorila";
+
+  # 必须和系统版本一致
+  home.stateVersion = "25.11";
+
+  # === 你的个人软件 ===
+  home.packages = with pkgs; [
+    fastfetch
+    btop
+    # 以后你想装 QQ、网易云、Spotify 都在这里加
+  ];
+
+  # === 你的 Git 配置 ===
+  programs.git = {
+    enable = true;
+    userName = "maorila";
+    userEmail = "maorila@qq.com";
+  };
+
+  # === 你的 Bash 配置 ===
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch";
+      ll = "ls -l";
+    };
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode;
+
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+      ms-python.python
+      ms-vscode-remote.remote-ssh
+    ];
+
+    userSettings = {
+      "editor.fontSize" = 16;
+      "editor.fontFamily" = "'Fira Code','Droid Sans Mono','monospace'";
+      "nix.enableLanguageServer" = true;
+      "files.autoSave" = "onFocusChange";
+    };
+  };
+
+  # 让 Home Manager 管理自己
+  programs.home-manager.enable = true;
+}
+
