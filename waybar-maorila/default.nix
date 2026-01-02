dcc75809fea2be4bacad3cb0c5119af8285db9d7
maorila <maorila@qq.com>
2026-01-02 14:43:49 +0800

    使用电池的waybar美化

---
+++ waybar-maorila/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  programs.waybar = {
+    enable = true;
+    # 这一行很重要，确保 waybar 能找到 systemd
+    systemd.enable = false;
+  };
+
+  # 直接把配置文件映射到 ~/.config/waybar/ 下
+  xdg.configFile."waybar/config.jsonc".source = ./config.jsonc;
+  xdg.configFile."waybar/style.css".source = ./style.css;
+}
